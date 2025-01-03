63a8a8c28fe22021e4afc15acfa59c2cd51433d8
Kronodile <pranavreddy27.m@gmail.com>
2025-01-03 23:02:08 +0530

    Render

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true 
 })
     .then(() => {
-        console.log('MongoDB connected');
+        console.log('MongoDB Atlas connected');
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
