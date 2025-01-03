b1368739ab07f49391629a308b4276c961f0ebed
Kronodile <pranavreddy27.m@gmail.com>
2025-01-03 23:31:17 +0530

    vercel #2

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
     origin: process.env.FRONTEND_URL,
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
